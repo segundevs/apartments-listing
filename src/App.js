002826a169b1b9f9ced1c1d12d6a550928b765a7
segundevs <tushbash@gmail.com>
2021-10-09 19:46:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Buy from './pages/Buy/Buy';
+import Sell from './pages/Sell/Sell';
+import Login from './pages/Login/Login';
+import Details from './pages/Details/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <Header />
+      <div className="app-container">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/buy">
+            <Buy />
+          </Route>
+          <Route path="/sell">
+            <Sell />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+        </Switch>
+      </div>
+   </Router>
   );
 }
 
 export default App;
+ 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import logo from '../../images/logo.png';
+import './header.style.scss'
+
+const Header = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+   <header className="header">
+      <nav className="navbar">
+       {/* <Link to="/" className="nav__brand"> */}
+        <img src={logo} alt="brand-logo" className="nav__brand"/>
+       {/* </Link> */}
+          
+
+      
+
+        <div className="navbar__links-container">
+
+        <div className="nav__links">
+          <Link to="/" className="nav__link">Home</Link>
+          <Link to="/buy" className="nav__link">Buy</Link>
+          <Link to="/sell" className="nav__link">Sell</Link>
+          <Link to="/login" className="nav__link login">Login</Link>
+        </div>
+
+        <div className="menu__icon" onClick={() => setIsOpen(prev => !prev)}>
+          {!isOpen ? <MenuIcon className="toggleIcon"/> : <CloseIcon className="toggleIcon"/>}
+        </div>
+
+        {isOpen && <div className="nav__links-mobile">
+          <Link to="/" className="nav__link" onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to="/buy" className="nav__link" onClick={() => setIsOpen(false)}>Buy</Link>
+          <Link to="/sell" className="nav__link" onClick={() => setIsOpen(false)}>Sell</Link>
+          <Link to="/login" className="nav__link login" onClick={() => setIsOpen(false)}>Login</Link>
+        </div>}
+
+
+       
+
+        </div>
+
+         
+      </nav>
+      
+    </header>
+  )
+}
+
+export default Header
