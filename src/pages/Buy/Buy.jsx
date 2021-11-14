40bd06f9451f952bf21623d87d147685ef75ba7c
segundevs <tushbash@gmail.com>
2021-11-14 10:53:32 -0800

    added listings page with search functionality

--- src/pages/Buy/Buy.jsx
+++ src/pages/Buy/Buy.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import Search from '../../components/Search/Search';
+import Card from '../../components/Card/Card';
+
+import { useData } from '../../contexts/dataContext';
+
+import './buy.style.scss';
 
 const Buy = () => {
+
+  const {data, getData} = useData();
+
+  useEffect(()=>{
+    getData()
+  }, [getData])
+  
   return (
-    <div>
-      Buy page
-    </div>
+    <React.Fragment>
+      <h4 className="buy-header">Search for properties...</h4>
+      <Search />
+      <div className="buy-container">
+        {data && data.map(apt => (
+          <Card apt={apt} key={apt._id} />
+        ))}  
+      </div>
+    </React.Fragment>
   )
 }
 
