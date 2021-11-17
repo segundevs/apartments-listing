729678120c65632811a25399b32c5089c76e2a65
segundevs <tushbash@gmail.com>
2021-11-17 00:32:34 -0800

    updated hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import visa from '../../images/visa-logo.png';
 import trustpilot from '../../images/trustpilot-logo.png';
 import './hero.style.scss';
 
-const Hero = ({location, setLocation, price, setPrice, type, setType}) => {
+const Hero = () => {
 
   return (
     <div className="hero__container">
@@ -22,58 +22,31 @@ const Hero = ({location, setLocation, price, setPrice, type, setType}) => {
 
        <div className="hero__search-container">
         <h4 className="hero__search-header">Search Homes</h4>
-        <form className="select__container">
-          {/* Location container */}
+        <div className="select__container">
           <div className="location">
             <h4 className="location__header">Location</h4>
-            <select className="location__select" value={location} onChange={(e)=>setLocation(e.target.value)}>
-            <option value="all">All</option>
-            <option value="abia">Abia</option>
-            <option value="adamawa">Adamawa</option>
-            <option value="akwa-ibom">Akwa-Ibom</option>
-            <option value="anambra">Anambra</option>
-            <option value="bauchi">Bauchi</option>
-            <option value="bayelsa">Bayelsa</option>
-            <option value="benue">Benue</option>
+            <select className="location__select" disabled >
+            <option value="abia">Lagos</option>
           </select>
           </div>
 
-          {/* Property type container */}
           <div className="type">
             <h4 className="type__header">Property Type</h4>
-            <select className="type__select" value={type} onChange={(e)=>setType(e.target.value)}>
-            <option value="all">All</option>
-            <option value="bungalow">Bungalow</option>
-            <option value="flat">Flat</option>
+            <select className="type__select" disabled >
             <option value="duplex">Duplex</option>
-            <option value="terrace">Terrace</option>
-            <option value="semi-detached">Semi-detached</option>
-            <option value="detached">Detached</option>
-            <option value="maisonette">Maisonette</option>
-            <option value="penthouse">Penthouse</option>
           </select>
           </div>
-
-          {/* Max Price */}
 
           <div className="price">
           <h4 className="price__header">Max Price</h4>
-            <select className="price__select" value={price} onChange={(e)=>setPrice(e.target.value)}>
-              <option value="all">All</option>
-              <option value="5000">$5000</option>
-              <option value="10000">$10,000</option>
-              <option value="15000">$15,000</option>
-              <option value="20000">$20,000</option>
-              <option value="250000">$25,000</option>
-              <option value="30000">$30,000</option>
-              <option value="400000">$40,000</option>
-              <option value=">40000">Above $40,0000</option>
+            <select className="price__select" disabled >
+              <option value="5000">$25000</option>
             </select>
         </div>
         <div className="search__icon">
           <SearchIcon />
         </div>
-        </form> 
+        </div> 
        </div>
       <div className="companies__icons">
         <img src={masterCard} alt="mastercard" />
