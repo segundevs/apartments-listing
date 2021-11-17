715e4046186950d8c61bdeaaae3d1e8902c08493
segundevs <tushbash@gmail.com>
2021-11-17 00:33:19 -0800

    added loading spinner for buttons

---
+++ src/components/ButtonLoader.jsx
@@ -0,0 +1,13 @@
+import { Oval } from 'react-loading-icons';
+
+const ButtonLoader = () => {
+   return (
+    <Oval 
+    stroke='hsl(167, 98%, 19%)'
+    height='1em'
+    strokeWidth={3}
+    />
+  )
+}
+
+export default ButtonLoader
