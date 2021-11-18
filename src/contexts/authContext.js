ed416b180363e48f4957068e26288931ef7ce20a
segundevs <tushbash@gmail.com>
2021-11-18 03:18:46 -0800

    updated authentication

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -20,8 +20,7 @@ const AuthProvider = ({children}) => {
   //Sign up
    const signUp = async (email, password, name) => {
     setLoading(false)
-      await
-    createUserWithEmailAndPassword(auth, email, password)
+    await createUserWithEmailAndPassword(auth, email, password)
     .then(response => {
       setUser(response.user)
       const user = auth.currentUser;
@@ -34,27 +33,18 @@ const AuthProvider = ({children}) => {
       return response.user
     })
     .catch(err => {
-      if(err.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).'){
-        setError('Password should be at least 6 characters')
-      }
-     else if(err.message === 'Firebase: The email address is already in use by another account. (auth/email-already-in-use).'){
-        setError('The email address is already in use by another account')
-      } 
-      else {
-        setError('There was a network error')
-      }
+      setError(err.message)
       setLoading(false)
       setTimeout(() => {
-            setError(false)
-          }, 2000)
-    }) 
+        setError(false)
+      }, 5000) 
+      }) 
   };
 
   //Login
   const login = async(email, password) => {
   setLoading(true)
-  await
-  signInWithEmailAndPassword(auth, email, password)
+  await signInWithEmailAndPassword(auth, email, password)
   .then(response => {
     setUser(response.user)
     setLoading(false)
@@ -63,26 +53,19 @@ const AuthProvider = ({children}) => {
     return response.user
   })
   .catch(err => {
-    if(err.message === 'Firebase: The password is invalid or the user does not have a password. (auth/wrong-password).'){
-      setError('The password is invalid or the user does not have a password')
-    }
-    else if (err.message === 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).'){
-      setError('There is no user with this email address or the user may have been deleted')
-    }
-    else {
-      setError(err.message)
-    }
-     setLoading(false)
+    console.log(err.message)
+    setError(err.message)
+    setLoading(false)
     setTimeout(() => {
-            setError(false)
-          }, 2000) 
+      setError(false)
+    }, 5000) 
   })
 }
 
 //Login with google
 const signInWithGoogle = async() => {
     setLoading(true)
-    signInWithPopup(auth, new GoogleAuthProvider())
+    await signInWithPopup(auth, new GoogleAuthProvider())
     .then(response => {
       setUser(response.user)
       setLoading(false)
@@ -98,22 +81,17 @@ const signInWithGoogle = async() => {
 
 //Send Password Reset Email
 const resetPassword = async(email) => {
-  await 
-  sendPasswordResetEmail(auth, email)
+  await sendPasswordResetEmail(auth, email)
   .then(() => {
     setLoading(false)
     toast.success('Please check your email to reset your password', {theme: "colored", autoClose: 2000 })
     return true;
   }).catch(err => {
-    if(err.message === 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).'){
-      setError('There is no user with this email address or the user may have been deleted')
-    } else {
-      setError(err.message)
-    } 
-      setLoading(false)
-      setTimeout(() => {
-        setError(false)
-      }, 2000)
+    setError(err.message)
+    setLoading(false)
+    setTimeout(() => {
+      setError(false)
+    }, 5000)
   })
 }
 
