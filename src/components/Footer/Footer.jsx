538e7b3640ae8579c3da961242b7f9574557211d
segundevs <tushbash@gmail.com>
2021-11-18 03:21:05 -0800

    formatted footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import logo from '../../images/logo.png';
-
 import './footer.style.scss';
 
 const Footer = () => {
