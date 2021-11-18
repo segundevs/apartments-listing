9c2e559e1a3e073bd3cba506e228fb0583974898
segundevs <tushbash@gmail.com>
2021-11-18 03:19:23 -0800

    updated data context

--- src/contexts/dataContext.js
+++ src/contexts/dataContext.js
@@ -1,4 +1,5 @@
-import {createContext, useContext, useState, useCallback} from 'react';
+import axios from 'axios';
+import {createContext, useContext, useState, useCallback, useEffect} from 'react';
 
 const DataContext = createContext();
 
@@ -7,32 +8,58 @@ export const useData = () => {
 }
 
 const DataProvider = ({children}) => {
-
-
   const [type, setType] = useState('all');
-  const [price, setPrice] = useState('all');
+  const [price, setPrice] = useState('');
   const [location, setLocation] = useState('');
   const [keyword, setKeyword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState('');
 
   const [data, setData] = useState([]);
 
-  const getData = useCallback(
-    () => {
-      fetch( `http://localhost:8080/api/apartments?q=${keyword}`)
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch(err => console.log(err.message))    
+  const url = 'http://localhost:8080/api/apartments';
+
+ const getData = useCallback(
+    async () => {
+      if(type === 'all' && price === '' && location === '' && keyword === ''){
+        setLoading(true)
+        try {
+          const res = await axios.get(url)
+          setData(res.data)
+          setLoading(false)
+        } catch (error) {
+          setErr(error.message)
+          setLoading(false)
+        }  
+      }else{
+        setLoading(false)
+        try {
+          const res = await axios.get(`${url}?q=${keyword}+${type}+${location}+${price}`)
+          setData(res.data)
+          setLoading(false)
+        } catch (error) {
+          setErr(error.message)
+          setLoading(false)
+        }  
+      }
     },
-    [keyword],
+    [type, location, price, keyword],
   )
+
+  useEffect(() => {
+    getData()
+  }, [getData])
 
 
   const values = {
+    url,
     type,
     price,
     location,
     keyword,
     data,
+    loading,
+    err,
     setType,
     setPrice,
     setLocation,
