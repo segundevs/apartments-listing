df03d0fcce83c3a926e8f94fbc2d45eff590118d
segundevs <tushbash@gmail.com>
2021-11-18 23:47:11 -0800

    updated context for data

--- src/contexts/dataContext.js
+++ src/contexts/dataContext.js
@@ -1,55 +1,59 @@
-import axios from 'axios';
-import {createContext, useContext, useState, useCallback, useEffect} from 'react';
+import axios from "axios";
+import {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+  useEffect,
+} from "react";
 
 const DataContext = createContext();
 
 export const useData = () => {
-  return useContext(DataContext)
-}
+  return useContext(DataContext);
+};
 
-const DataProvider = ({children}) => {
-  const [type, setType] = useState('all');
-  const [price, setPrice] = useState('');
-  const [location, setLocation] = useState('');
-  const [keyword, setKeyword] = useState('');
+const DataProvider = ({ children }) => {
+  const [type, setType] = useState("all");
+  const [price, setPrice] = useState("");
+  const [location, setLocation] = useState("");
+  const [keyword, setKeyword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [err, setErr] = useState('');
+  const [err, setErr] = useState("");
 
   const [data, setData] = useState([]);
 
-  const url = 'http://localhost:8080/api/apartments';
+  const url = "https://apatmentshub.herokuapp.com/api/apartments";
 
- const getData = useCallback(
-    async () => {
-      if(type === 'all' && price === '' && location === '' && keyword === ''){
-        setLoading(true)
-        try {
-          const res = await axios.get(url)
-          setData(res.data)
-          setLoading(false)
-        } catch (error) {
-          setErr(error.message)
-          setLoading(false)
-        }  
-      }else{
-        setLoading(false)
-        try {
-          const res = await axios.get(`${url}?q=${keyword}+${type}+${location}+${price}`)
-          setData(res.data)
-          setLoading(false)
-        } catch (error) {
-          setErr(error.message)
-          setLoading(false)
-        }  
+  const getData = useCallback(async () => {
+    if (type === "all" && price === "" && location === "" && keyword === "") {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        setErr(error.message);
+        setLoading(false);
       }
-    },
-    [type, location, price, keyword],
-  )
+    } else {
+      setLoading(false);
+      try {
+        const res = await axios.get(
+          `${url}?q=${keyword}+${type}+${location}+${price}`
+        );
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        setErr(error.message);
+        setLoading(false);
+      }
+    }
+  }, [type, location, price, keyword]);
 
   useEffect(() => {
-    getData()
-  }, [getData])
-
+    getData();
+  }, [getData]);
 
   const values = {
     url,
@@ -65,14 +69,10 @@ const DataProvider = ({children}) => {
     setLocation,
     setKeyword,
     setData,
-    getData
-  }
+    getData,
+  };
 
-  return (
-    <DataContext.Provider value={values}>
-      {children}
-    </DataContext.Provider>
-  )
-}
+  return <DataContext.Provider value={values}>{children}</DataContext.Provider>;
+};
 
 export default DataProvider;
