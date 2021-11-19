933199e5c0d1b0ff82acd8b035b3169beb40a8d5
segundevs <tushbash@gmail.com>
2021-11-18 23:48:47 -0800

    added loading spinner

--- src/pages/Buy/Buy.jsx
+++ src/pages/Buy/Buy.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Search from '../../components/Search/Search';
 import Card from '../../components/Card/Card';
-
+import AuthError from '../../components/AuthError/AuthError';
 import { useData } from '../../contexts/dataContext';
-
 import './buy.style.scss';
+import PageLoader from '../../components/PageLoader';
 
 const Buy = () => {
 
-  const { data } = useData();
+  const { data, loading, err } = useData();
 
   return (
     <React.Fragment>
       <h4 className="buy-header">Search for properties...</h4>
       <Search />
       <div className="buy-container">
+        {loading && <PageLoader />}
+        {err && <AuthError component={err} />}
         {data && data.map(apt => (
           <Card apt={apt} key={apt._id} />
         ))}  
