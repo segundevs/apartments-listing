cd0739b65cf790cf7ba5d4b6b51c331444706790
segundevs <tushbash@gmail.com>
2021-11-19 00:22:16 -0800

    updated details page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -11,7 +11,7 @@ const Details = () => {
 
   useEffect(() => {
     const getListing = async  () => {
-      const res = await axios.get(`http://localhost:8080/api/apartments/${id}`)
+      const res = await axios.get(`https://apatmentshub.herokuapp.com/api/apartments/${id}`)
       setListing(res.data) 
     }
     getListing()
