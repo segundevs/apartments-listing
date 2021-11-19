7a685bca1340240669568b79c44dbd6073fc213f
segundevs <tushbash@gmail.com>
2021-11-18 23:49:52 -0800

    updated user dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,25 +6,26 @@ import EditIcon from '@mui/icons-material/Edit';
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-
-import  './dashboard.style.scss';
-
 import { getStorage, deleteObject, ref } from "firebase/storage";
 import { auth } from '../../firebase';
+import  './dashboard.style.scss';
 
 const Dashboard = () => {
   const { user } = useAuth();
-  const { getData, data } = useData();
+  const { getData, data, url } = useData();
   const history = useHistory();
   const storage = getStorage();
 
   const [currentUserData, setCurrentUserData] = useState([]);
 
+  //Filter thru the data and return only those listings by the logged in user
   useEffect(() => {
     const response = data.filter(res => res.userId === user.uid);
     setCurrentUserData(response);
   }, [data, user.uid])
 
+
+  //Get a reference to the particular image file from firebase storage
    const deleteImage = async (fileName) => {
       if(!fileName) return
       const httpsReference = ref(storage, fileName);
@@ -32,19 +33,23 @@ const Dashboard = () => {
       await deleteObject(desertRef);
   }
 
+  //Make api request to delete data
   const deleteData = async(id) => {
     const token = await auth.currentUser.getIdToken();
-    await axios.delete(`http://localhost:8080/api/apartments/${id}`, { headers: {
+    await axios.delete(`${url}/${id}`, { headers: {
       'Authorization': `Bearer ${token}`
     }});
   }
   
+  //Delete data and file on clicking the delete button
   const handleDelete = async(apt) => {
     deleteImage(apt.imageUrl);
     await deleteData(apt._id);
     getData();
   }
 
+  //Take user to sell page and populate the input fields with 
+  // the data of the listing that is to be edited
   const handleEdit = (apt) => {
     history.push(`/sell/${apt._id}`)
   }
@@ -54,6 +59,7 @@ const Dashboard = () => {
     <div className="dashboard__container">
       <h2>{`${user.displayName}'s dashboard`}</h2>
       <h4>{`Welcome ${user.displayName}, ${currentUserData.length > 0 ? 'view your current listings below' : 'you currently do not have any listing'}`}</h4>
+      {/* If current user has listing, display them, else display null */}
       {currentUserData ? currentUserData.map(apt => (
         <div className="dashboard-card" key={apt._id}>
           <div className="dashboard__details">
@@ -64,7 +70,6 @@ const Dashboard = () => {
             <ReadMoreIcon className="more-icon" onClick={() => history.push(`details/${apt._id}`)} />
           </div>
         </div>
-
       )) : null}
     </div>
   )
